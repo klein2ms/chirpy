85c6f6afe728787c98a003aaa8203cf2317edf9c
Sean Klein <klein2ms@gmail.com>
2026-02-17 09:23:34 -0500

    ✨ Introduce content filter to api

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"slices"
+	"strings"
 	"sync/atomic"
 )
 
@@ -39,18 +43,53 @@ func main() {
 		})
 
 	mux.HandleFunc(
-		"GET /api/metrics",
+		"GET /admin/metrics",
 		func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+			w.Header().Set("Content-Type", "text/html; charset=utf-8")
 			w.WriteHeader(http.StatusOK)
-			_, _ = w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+			_, _ = w.Write([]byte(fmt.Sprintf(
+				"<html>\n  <body>\n    <h1>Welcome, Chirpy Admin</h1>\n    <p>Chirpy has been visited %d times!</p>\n  </body>\n</html>", cfg.fileserverHits.Load())))
 		})
 
 	mux.HandleFunc(
-		"POST /api/reset",
+		"POST /admin/reset",
 		func(w http.ResponseWriter, r *http.Request) {
 			cfg.resetFileServerHits()
 			w.WriteHeader(http.StatusOK)
+		})
+
+	mux.HandleFunc(
+		"POST /api/validate_chirp",
+		func(w http.ResponseWriter, r *http.Request) {
+			var body Body
+			err := json.NewDecoder(r.Body).Decode(&body)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			defer func(Body io.ReadCloser) {
+				_ = Body.Close()
+			}(r.Body)
+
+			var statusCode int
+			var content string
+
+			if len(body.Body) > 140 {
+				fmt.Printf("Content-Length: %d\n", len(body.Body))
+				statusCode = http.StatusBadRequest
+				content = "{\"error\": \"Chirp is too long\"}"
+			} else {
+				content = fmt.Sprintf("{\"cleaned_body\": \"%s\"}", contentFilter(body.Body))
+				statusCode = http.StatusOK
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(statusCode)
+			_, err = w.Write([]byte(content))
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				_, _ = w.Write([]byte("{\"error\": \"Something went wrong\"}"))
+			}
 		})
 
 	server := &http.Server{
@@ -59,4 +98,22 @@ func main() {
 	}
 
 	_ = server.ListenAndServe()
+}
+
+func contentFilter(content string) string {
+	words := strings.Fields(content)
+
+	badWords := []string{"kerfuffle", "sharbert", "fornax"}
+
+	for i, word := range words {
+		if slices.Contains(badWords, strings.ToLower(word)) {
+			words[i] = "****"
+		}
+	}
+
+	return strings.Join(words, " ")
+}
+
+type Body struct {
+	Body string `json:"body"`
 }
